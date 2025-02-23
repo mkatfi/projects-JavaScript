f91ff20c8b23c931528898e147bd9ac02704fb41
Marouane Katfi <mkatfi@c3r10p8.1337.ma>
2025-02-23 03:09:22 +0000

    weater fi

--- Weather-App/webpack.config.js
+++ Weather-App/webpack.config.js
@@ -1,3 +1,4 @@
+
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
@@ -14,14 +15,14 @@ module.exports = {
     static: {
       directory: path.resolve(__dirname, "public"),
     },
-    watchFiles: ["./public/index.html", "./src/**/*.js"],
+    watchFiles: ["./index.html", "./src/**/*.js"],
     compress: true,
-    port: 8080,
+    port: 3080,
     hot: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: "./index.html",
     }),
   ],
   module: {
